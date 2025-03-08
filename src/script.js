4e8bd15d7c25d021871be19f9698fe4bc28a3fb8
Karen Luna <karenlunafeldheim@gmail.com>
2025-03-08 19:53:46 -0300

    Añadir grilla para el juego

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+// Color del tema
+
 document.querySelector("#dark-mode-toggle").addEventListener("click", () => {
   document.body.classList.toggle("dark");
   const isDarkMode = document.body.classList.contains("dark");
@@ -9,11 +11,25 @@ document.querySelector("#dark-mode-toggle").addEventListener("click", () => {
     .setAttribute("content", isDarkMode ? "#1a1a2e" : "#fff");
 });
 
+//==========================================
+
+let level_index = 0;
+let level = CONSTANT.LEVEL[level_index];
+
+document.querySelector("#btn-level").addEventListener("click", () => {
+  level_index =
+    level_index + 1 > CONSTANT.LEVEL.length - 1 ? 0 : level_index + 1;
+  level = CONSTANT[level_index];
+  e.target.innerHTML = CONSTANT.LEVEL_NAME[level_index];
+});
+
+// =========================================
+
 const init = () => {
-  const darkmode = localStorage.getItem("darkmode");
-  document.body.classList.add(darkmode ? "dark" : "");
+  const darkmode = JSON.parse(localStorage.getItem("darkmode"));
+  document.body.classList.add(darkmode ? "dark" : "light");
   document
-    .querySelector('meta[name="theme-color"')
+    .querySelector('meta[name="theme-color"]')
     .setAttribute("content", darkmode ? "#1a1a2e" : "#fff");
 };
 
